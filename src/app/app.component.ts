4ef39820133e8002aa1d3d71abcaf59c234015a4
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2018-08-15 20:39:41 +0900

    新規追加

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div>Pipe</div>
+  `
+})
+export class AppComponent {
+  /** */
+  memo: string = '';
+}
